b111762ec0abe9a2aefd03746ae9a00341a4dfd8
Samael-24 <parishk1024@gmail.com>
2023-08-01 16:26:52 +0530

    Horly update Done

--- src/components/WeatherDetails/WeatherDeatils.jsx
+++ src/components/WeatherDetails/WeatherDeatils.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { motion } from "framer-motion";
 import styles from "./WeatherDetails.module.css";
 import {
   FaSun,
@@ -13,13 +14,38 @@ import {
   FaWind,
   FaEye,
   FaCloudRain,
+  FaCloud,
 } from "react-icons/fa6";
 import { TbUvIndex } from "react-icons/tb";
 import { WiHumidity } from "react-icons/wi";
 import { GiSunRadiations } from "react-icons/gi";
 
 const WeatherDeatils = ({ weatherInfo }) => {
+  console.log(weatherInfo.days[0].hours);
   const icon = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/2de560da89d87de44e3ca2a6593a12c19c8346d3/SVG/2nd%20Set%20-%20Color/${weatherInfo.days[0].icon}.svg`;
+  const hourly = weatherInfo.days[0].hours;
+  const hourUpdates = hourly.map((value, index) => {
+    const icons = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/2de560da89d87de44e3ca2a6593a12c19c8346d3/SVG/2nd%20Set%20-%20Color/${value.icon}.svg`;
+    return (
+      <motion.div key={index} className={styles.hourUpdates}>
+        <div>
+          {value.temp}
+          <sup>&deg;C</sup>
+        </div>
+        <div>
+          <img
+            src={icons}
+            alt=""
+            height="50px"
+            width="50px
+          "
+          />
+        </div>
+        <div>{value.datetime}</div>
+      </motion.div>
+    );
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.tempShow}>
@@ -27,10 +53,12 @@ const WeatherDeatils = ({ weatherInfo }) => {
           {weatherInfo.currentConditions.temp}
           <sup>&deg;C</sup>
         </span>
-        <span>
-          <img src={icon} alt="" height="150px" width="150px" />
+        <span className={styles.iconic}>
+          <img src={icon} alt="" height="200px" width="200px" />
         </span>
-        <p>{weatherInfo.currentConditions.conditions}</p>
+        <p className={styles.condition}>
+          Conditions: {weatherInfo.currentConditions.conditions}
+        </p>
         <hr />
         <p>
           <FaMapLocationDot style={{ margin: "4px" }} />
@@ -65,7 +93,7 @@ const WeatherDeatils = ({ weatherInfo }) => {
               Humidity: {weatherInfo.currentConditions.humidity}
             </p>
             <p>
-              <FaWind style={{ marginRight: "4px" }} />
+              <FaWind style={{ marginRight: "8px" }} />
               Wind Speed: {weatherInfo.currentConditions.windspeed} km/h
             </p>
             <p>
@@ -88,19 +116,35 @@ const WeatherDeatils = ({ weatherInfo }) => {
           <div className={styles.temperatureDetails}>
             <h4 className={styles.headings}>Temperature Details</h4>
             <p>
-              <FaTemperatureHigh />
+              <FaTemperatureHigh style={{ marginRight: "8px" }} />
               Temperature Max: {weatherInfo.days[0].tempmax}
               <sup>&deg;C</sup>
             </p>
             <p>
-              <FaTemperatureLow />
+              <FaTemperatureLow style={{ marginRight: "8px" }} />
               Temperature Min: {weatherInfo.days[0].tempmin}
               <sup>&deg;C</sup>
             </p>
           </div>
         </div>
       </div>
-      {/* <div className={styles.weatherDetails}>Weatherdetails</div> */}
+      <div className={styles.HourDetails}>
+        <h3
+          style={{ borderBottom: "1px solid aqua", padding: "8px 0px 8px 0px" }}
+        >
+          <i>Hourly forecast</i>
+        </h3>
+        <motion.div whileTap={"grabbing"}>
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 500, left: -500 }}
+            className={styles.hour}
+          >
+            {hourUpdates}
+          </motion.div>
+        </motion.div>
+        {/* <ul className={styles.hour}>{hourUpdates}</ul> */}
+      </div>
       {/* <div>Hourlydetails</div>
       <div>dayForcast15-</div> */}
     </div>
